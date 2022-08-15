9ca9ff1d0d2bba75645f544574b4aa3a9af29f07
Ivona Maksimovic <ivonamaksimovic05@gmail.com>
2022-08-15 09:18:50 +0200

    Added media queries to Card component

--- components/Card/style.js
+++ components/Card/style.js
@@ -1,23 +1,51 @@
 import styled from 'styled-components';
 
+const breakpoints = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+};
+
 const Container = styled.div`
   position: relative;
   margin-top: 1rem;
   margin-bottom: 1rem;
   border: ${(props) =>
     props.title ? 'solid 2px var(--clr-dark-blue)' : 'solid 1px #bfbfbf'};
-  width: 60rem;
+  width: 90%;
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    width: 55rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    width: 60rem;
+  }
 `;
 
 const CardTitle = styled.div`
   position: absolute;
   top: -15px;
-  left: 15px;
   background: ${(props) => (props.title ? 'var(--clr-dark-blue)' : '#bfbfbf')};
   border-radius: 3px;
-  padding: 0.1rem 0.5rem;
-  font-size: 0.85rem;
+  padding: 0.1rem 0.2rem;
+  font-size: 0.75rem;
   font-weight: 700;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    left: 15px;
+    padding: 0.1rem 0.3rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    font-size: 0.8rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    padding: 0.1rem 0.5rem;
+    font-size: 0.85rem;
+  }
 `;
 
 const CardTitleLink = styled.a`
@@ -30,18 +58,50 @@ const Content = styled.div`
 `;
 
 const ContentContainer = styled.div`
-  padding-left: 7.5rem;
+  margin-top: 0.5rem;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    margin-top: 0;
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    padding-left: 8rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    padding-left: 7.5rem;
+  }
 `;
 
 const NoteText = styled.p`
-  font-size: 0.75rem;
+  font-size: 0.65rem;
   color: grey;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    font-size: 0.7rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    font-size: 0.75rem;
+  }
 `;
 
 const InsuranceDeal = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
 const ImageContainer = styled.div`
@@ -50,29 +110,59 @@ const ImageContainer = styled.div`
 
 const DealBenefits = styled.div`
   display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    flex-direction: row;
+  }
 `;
 
 const Deal = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  text-align: center;
   width: 14rem;
-  margin-right: 0.25rem;
-  margin-left: 0.25rem;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
-  padding-top: 0.25rem;
-  padding-bottom: 1rem;
+  margin: 0.25rem;
+  padding: 0.25rem 0.5rem 1rem 0.5rem;
   background: #fcf3f8;
   border-bottom: solid 4px #cc6aa0;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    width: 9rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    width: 12rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    width: 14rem;
+  }
 `;
 
 const DealSmallText = styled.span`
   font-size: 0.7rem;
 `;
 const DealBigText = styled.span`
-  font-size: 1rem;
+  font-size: 0.85rem;
   padding-top: 0.5rem;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    font-size: 0.9rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    font-size: 0.95rem;
+  }
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    font-size: 1.05rem;
+  }
 `;
 
 const DropdownContent = styled.div`
@@ -110,12 +200,20 @@ const DealInfo = styled.div`
 `;
 
 const DealInfoName = styled.p`
-  font-size: 0.8rem;
+  font-size: 0.7rem;
   font-weight: 700;
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    font-size: 0.8rem;
+  }
 `;
 
 const DealInfoValue = styled.p`
-  font-size: 0.8rem;
+  font-size: 0.7rem;
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    font-size: 0.8rem;
+  }
 `;
 
 export {
